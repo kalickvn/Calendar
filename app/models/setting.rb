bb225fc1dc1a957d65c47d05ab347b53ee71cc9e
= <=>
2014-09-24 15:31:32 +0700

    commit for fix bug

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -2,7 +2,10 @@
 class Setting < ActiveRecord::Base
   CODE_ATTR = {
     :PROMOTE_NEWS => "PROMOTE",
-    :EMAIL_FEED => "EMAIL_FEED"
+    :EMAIL_FEED => "EMAIL_FEED",
+    :HOME_INTRO => "HOME_INTRO",
+    :INTRO_FIELD => "INTRO",
+    :PRICING_FIELD => "PRICING"
   }
   attr_accessible :code,:value
 end
