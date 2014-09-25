dd7449d3bdd8f984df16d789bb7ec4a00dba06d8
= <=>
2014-09-25 14:03:01 +0700

    commit

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,5 +3,5 @@ class Category < ActiveRecord::Base
   attr_accessible :name,:description,:is_show_menu,:order_no
   validates_presence_of :name, :message =>"Tên loại lịch không được bỏ trống."
   validates_presence_of :description 
-  has_many :calendars 
+  has_many :calendars,:dependent => :destroy
 end
